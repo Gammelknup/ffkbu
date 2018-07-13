0c6cca3158d515dc306122bd461134b83ec6f4f2
Simon Müller <simon.f.mueller@gmail.com>
2018-07-13 08:04:27 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,27 +1,40 @@
 ##      gluon site.mk makefile example
 
-##      GLUON_SITE_PACKAGES
-#               specify gluon/openwrt packages to include here
-#               The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+##	GLUON_FEATURES
+#		Specify Gluon features/packages to enable;
+#		Gluon will automatically enable a set of packages
+#		depending on the combination of features listed
+
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	mesh-batman-adv-15 \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard
+
+##	GLUON_SITE_PACKAGES
+#		Specify additional Gluon/LEDE packages to include here;
+#		A minus sign may be prepended to remove a packages from the
+#		selection that would be enabled by default or due to the
+#		chosen feature flags
 
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-15 \
-        gluon-respondd \
-        gluon-autoupdater \
         gluon-config-mode-autoupdater \
         gluon-config-mode-contact-info \
         gluon-config-mode-geo-location \
         gluon-config-mode-hostname \
         gluon-config-mode-core \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-limit-arp \
         gluon-radv-filterd \
         respondd-module-airtime \
         gluon-web-admin \
         gluon-web-autoupdater \
         gluon-web-wifi-config \
         gluon-web-network \
-        gluon-status-page \
         haveged \
         iptables \
         iwinfo \
@@ -211,15 +224,14 @@ GLUON_TLWDR4900_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_P
 DEFAULT_GLUON_RELEASE := v2018.1-JohnnyBee
 
 
-##      GLUON_RELEASE
-#               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
-#               e.g.:
-#                       $ make images GLUON_RELEASE=23.42+5
-#               would generate images named like this:
-#                       gluon-ff%site_code%-23.42+5-%router_model%.bin
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
 
-# Allow overriding the release number from the command line
-GLUON_RELEASE ?= v2018.1-JohnnyBee
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
